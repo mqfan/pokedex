d26a889e66357ecc9105801a0bc6ca09b785dd09
Michael Fan <michael.fan@berkeley.edu>
2017-03-03 03:12:32 -0800

    first commit

---
+++ PokedexLab/TypeViewController.swift
@@ -0,0 +1,11 @@
+//
+//  typeViewController.swift
+//  PokedexLab
+//
+//  Created by Michael Fan on 3/2/17.
+//  Copyright © 2017 iOS Decal. All rights reserved.
+//
+
+import UIKit
+
+class TypeViewController: UICollection
